7ed64b5c5f3e4351897cf72b09a1502646936ae3
kainthekiller <phillipyates12@hotmail.com>
2022-08-28 20:45:13 -0500

    Did some more fixes got martian Attack Graphic box to show up at the right location

--- main.js
+++ main.js
@@ -19,13 +19,13 @@ camera.position.setX(-3);
 
 renderer.render(scene, camera);
 
-// Torus
+// Spinning Donut
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0x0d175e });
-const torus = new THREE.Mesh(geometry, material);
+const spinningDonut = new THREE.Mesh(geometry, material);
 
-scene.add(torus);
+scene.add(spinningDonut);
 
 // Lights
 
@@ -61,16 +61,26 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('midj.png');
+const spaceTexture = new THREE.TextureLoader().load('MidJ.png');
 scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('me.png');
+const myProfilePicFile = new THREE.TextureLoader().load("me.png");
 
-const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
+const myPic = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: myProfilePicFile }));
 
-scene.add(jeff);
+scene.add(myPic);
+
+// Martian Attack Box Image--------------------------------------------
+
+const martianAttackFile = new THREE.TextureLoader().load("../Pic/Martian Attack/Ma2.png");
+
+const martianAttack1 = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: martianAttackFile }));
+
+scene.add(martianAttack1);
+martianAttack1.position.z = 20;
+
 
 // Moon
 
@@ -90,8 +100,8 @@ scene.add(moon);
 moon.position.z = 30;
 moon.position.setX(-10);
 
-jeff.position.z = -5;
-jeff.position.x = 2;
+myPic.position.z = -5;
+myPic.position.x = 2;
 
 // Scroll Animation
 
@@ -101,8 +111,12 @@ function moveCamera() {
   moon.rotation.y += 0.075;
   moon.rotation.z += 0.05;
 
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
+ martianAttack1.rotation.x += 0.05;
+ martianAttack1.rotation.y += 0.075;
+ martianAttack1.rotation.z += 0.05;
+
+  myPic.rotation.y += 0.01;
+  myPic.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
@@ -117,9 +131,12 @@ moveCamera();
 function animate() {
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.11;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.11;
+  spinningDonut.rotation.x += 0.01;
+  spinningDonut.rotation.y += 0.005;
+  spinningDonut.rotation.z += 0.01;
+
+  myPic.rotation.y += 0.01;
+  myPic.rotation.x += 0.01;
 
   moon.rotation.x += 0.005;
 
